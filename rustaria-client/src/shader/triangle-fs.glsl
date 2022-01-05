5c5ef9ef1c00dab059e991ee54fe3014fbbed2af
alphaqu <yan.gyunashyan@gmail.com>
2022-01-05 12:12:40 +0100

    naming guidelines...

--- rustaria-client/src/shader/triangle-fs.glsl
+++ rustaria-client/src/shader/triangle-fs.glsl
@@ -1,7 +1,7 @@
 #version 440
 
-layout(location = 0) out vec4 out_Color;
+layout(location = 0) out vec4 f_out_Color;
 
 void main() {
-    out_Color = vec4(0.3, 0.2, 0.1, 1.0);
+    f_out_Color = vec4(0.3, 0.2, 0.1, 1.0);
 }
