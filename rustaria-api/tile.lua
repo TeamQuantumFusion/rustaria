1bac1aa5630a6496910b9dfd6462541029715e3b
leocth <22406910+leocth@users.noreply.github.com>
2022-01-10 20:48:48 +0800

    feat(api): full EmmyLua annotations for API

--- rustaria-api/tile.lua
+++ rustaria-api/tile.lua
@@ -1,6 +1,6 @@
 ---Registers a tile to Rustaria.
 ---
----@param entries table table of tiles, with string keys
+---@param entries table<string, tile>
 ---@return nil
 local function register(entries)
     -- builtin stub
@@ -8,10 +8,32 @@ end
 
 ---Creates a default tile
 ---
----@param entries table
----@return userdata
-local function default(entries)
+---@param settings default_tile_settings
+---@return tile
+local function default(settings)
     -- builtin stub
 end
+
+-- Type annotations
+---@alias blast_resistance number | '"indestructible"'
+---@alias break_resistance '"any"' | '"indestructible"' | { axe: number } | { pickaxe: number } | { hammer: number }
+---@alias dynamic_bool '"dynamic"' | { fixed: boolean }]
+---@alias tile_filter '"all"' | '"none"' | { whitelist: tag[] } | { blacklist: tag[] }
+---@alias tile_type '"default"' | { type: '"spreadable"', spread_chance: number, filter: tile_filter }
+---@alias tile userdata
+local dummy = {}
+
+---@class tag
+---@field tag string
+---@field category '"tile"' | '"item"' | '"wall"' | '"tree"'
+local tag = {}
+
+---@class default_tile_settings
+---@field transitional boolean
+---@field collision dynamic_bool
+---@field opaque dynamic_bool
+---@field blast_resistance blast_resistance
+---@field break_resistance break_resistance
+local default_tile_settings = {}
 
 return {register = register, default = default}
